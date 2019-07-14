c37214cfabbb26759c47928a2f5dabc503397ee8
Stephen Standridge <stephen@formisfunction.io>
2019-07-14 00:57:13 -0700

    Update WalabotOSC.py

--- walabot/WalabotOSC.py
+++ walabot/WalabotOSC.py
@@ -2,7 +2,7 @@
 OSC client that transmits walabot data over a network
 """
 import os
-from dotenv import dotenv
+from dotenv import Dotenv
 dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 os.environ.update(dotenv)
 import argparse
